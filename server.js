c8c05cfc690b5b6dab2b3f0c0c6af00c75edee3d
Thoyt810 <66744198+Thoyt810@users.noreply.github.com>
2020-07-23 14:35:56 -0600

    bottom clicks

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
-// Dependencies
-// =============================================================
 const express = require("express");
 const path = require("path");
-
-// Sets up the Express App
-// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
 const reservations = [];
 
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -34,39 +19,23 @@ app.get("/reserve", function (req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-// Displays all characters
-app.get("/api/reservations", function (req, res) {
+app.get("/waitlist", function(req, res) {
+  return res.json(reservations[i > 5])
+})
+
+app.get("/reservations", function (req, res) {
   return res.json(reservations);
 });
 
-// Displays a single character, or returns false
-app.get("/api/reservations/:id", function (req, res) {
-  const chosen = req.params.reservations;
+app.post("/api/reservations", function (req, res) {
 
-  console.log(chosen);
+  var newReservation = req.body;
 
-  for (var i = 0; i < reservations.length; i++) {
-    if (chosen === reservations[i].routeName) {
-      return res.json(reservations[i]);
-    }
-  }
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
 
-  return res.json(false);
-});
+  console.log(newReservation);
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function (req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCharacter);
-
-  characters.push(newCharacter);
+  reservations.push(newReservation);
 
   res.json(newCharacter);
 });
